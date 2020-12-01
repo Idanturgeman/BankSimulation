1030f9775185ed20c7c75f10f6e4a82e1d1ea565
Idanturjeman <idan_turjeman29@walla.co.il>
2020-12-01 18:35:39 +0200

    finish, last commit

--- main.c
+++ main.c
@@ -6,65 +6,87 @@
 
 #include <stdio.h>
 
-void printMenu(){
+void printMenu()
+{
   printf("\t-Help menu- \n\nO: Open new account \nB: Print balance \nD: Deposite  \nW: Withrawal \nC: Close account \n");
   printf("I: Add interest to all accounts \nP: Print all open accounts and balance \nE: Close all accounts and exit \nH: Help\n\n");
+
 }
 
-void putInt(int *x){ // make sure that user insert an int
-  while(!scanf(" %d", x)){
+void putInt(int *x)
+{ // make sure that user insert an int
+  while(!scanf(" %d", x))
+{
     getchar();
     printf("Pleas enter an integer number\n");
   }
 }
-void putDouble(double *x){ // make sure that user insert a double
-  while(!scanf(" %lf", x)){
+void putDouble(double *x)
+{ // make sure that user insert a double
+  while(!scanf(" %lf", x))
+{
       getchar();
       printf("Pleas enter a real number\n");
   }
 }
- int main() {
+ int main() 
+{
   printf("Welcome to my online bank \nfor help in any time press H\n\n");
   bool exit = false;
-  while (!exit) {
+  while (!exit) 
+    {
     printMenu();
     printf("Transaction type?\n");
     char act;
     scanf(" %c", &act);
-    switch (act) {
-      case 'O':{
+    switch (act) 
+    {
+      case 'O':
+    {
         printf("Initial deposite?\n");
         double amount;
         putDouble(&amount);
         amount = round2Digits(amount);
         int ans = openAcc(amount);
         if(ans != WRONG_INDX)
+         {
           printf("Your account number is %d and your balance is %g\n", ans, getBalance(ans));
+         }
         else
+        {
           printf("I'm sorry but we don't have a place for you. try later.\n");
+        }
         break;
       }
-      case 'B':{
+      case 'B':
+       {
 	int account;      
         printf("Account number?\n");
         putInt(&account);
-	if(!(isActive(account))){
+	if(!(isActive(account)))
+       {
                         printf("%d account number does'nt exist\n",account);
                         break;
                         }
 
         double ans = getBalance(account);
         if(ans != WRONG_INDX)
+         {
           printf("The balance for %d account is %g\n", account, ans);
+        }
         else
+         {
           printf("Account dosn't exist\n");
-        break;
+        }
+         break;
       }
-      case 'D':{
+      case 'D':
+       {
         int account;
         printf("Account number?\n");
         putInt(&account);
-	if(!(isActive(account))){
+	if(!(isActive(account)))
+        {
 			printf("%d account number does'nt exist\n",account);
 			break;
 			}
@@ -74,16 +96,22 @@ void putDouble(double *x){ // make sure that user insert a double
         amount = round2Digits(amount);
         double ans = deposite(account, amount);
         if(ans != WRONG_INDX)
+         {
           printf("%g was deposite. the balance for %d account is %g\n", amount, account, ans);
+        }
         else
+        {
           printf("Account dosn't exist OR you entered a negativ amount\n");
-        break;
+        }
+       break;
       }
-      case 'W':{
+      case 'W':
+       {
         int account;
         printf("Account number?\n");
         putInt(&account);
-	if(!(isActive(account))){
+	if(!(isActive(account)))
+        {
                         printf("%d account number does'nt exist\n",account);
                         break;
                         }
@@ -94,28 +122,39 @@ void putDouble(double *x){ // make sure that user insert a double
         amount = round2Digits(amount);
         double ans = withrawal(account, amount);
         if(ans != WRONG_INDX)
+         {
           printf("%g was withrawaled. the balance for %d account is %g\n", amount, account, ans);
+        }
         else
+        {
           printf("Account dosn't exist OR you don'e have enough money OR you entered a negativ amount\n");
+        }
         break;
       }
-      case 'C':{
+      case 'C':
+       {
         int account;
         printf("Account number?\n");
         putInt(&account);
-	if(!(isActive(account))){
+	if(!(isActive(account)))
+        {
                         printf("%d account number does'nt exist\n",account);
                         break;
                         }
 
         bool ans = closeAcc(account);
         if(ans)
+         {
           printf("Account %d is close succsesfuly\n", account);
+        }
         else
+        {
           printf("Wrong account number\n");
-        break;
+        }
+       break;
       }
-      case 'I':{
+      case 'I':
+       {
         double inter;
         printf("Interest rate?\n");
         putDouble(&inter);
@@ -123,24 +162,29 @@ void putDouble(double *x){ // make sure that user insert a double
         printf("Interest was add with rate of %g\n", inter);
         break;
       }
-      case 'P':{
+      case 'P':
+      {
         printAll();
         break;
       }
       case 'E':{
+      
         closeAll();
         exit = true;
         printf("The bank is close. Goodbye!");
         break;
       }
-      case 'H':{
+      case 'H':
+      {
         printMenu();
         break;
       }
-      default:{
+      default:
+      {
         printf("'%c' is wrong input. try again (H for help).\n", act);
       }
     }
     printf("\n");
   }
 }
+
--- myBank.c
+++ myBank.c
@@ -1,84 +1,77 @@
-#ifndef FIELDS
-#define FIELDS 2
-#endif
+
 
 #ifndef ACCOUNTS
 #define ACCOUNTS 50
 #endif
 
-#ifndef FIRST_ACC
-#define FIRST_ACC 901
+#ifndef OPEN_ACCOUNT
+#define OPEN_ACCOUNT 1
 #endif
+
+#ifndef CLOSE_ACCOUNT
+#define CLOSE_ACCOUNT 0
+#endif
+
+
+#ifndef ACCOUNT_STATUS
+#define ACCOUNT_STATUS 0
+#endif
+
+
+#ifndef ACCOUNT_BALANCE
+#define ACCOUNT_BALANCE 1
+#endif
+
+#ifndef FIRST_ACCOUNT
+#define FIRST_ACCOUNT 901
+#endif
+
 
 #ifndef WRONG_INDX
 #define WRONG_INDX -1
 #endif
 
-#ifndef OPEN
-#define OPEN 1
-#endif
-
-#ifndef CLOSE
-#define CLOSE 0
-#endif
-
-#ifndef ACC_STT
-#define ACC_STT 0
-#endif
-
-#ifndef ACC_BLC
-#define ACC_BLC 1
-#endif
 
 #include <stdbool.h>
 #include <stdio.h>
+#include "myBank.h"
 
-double acc[FIELDS][ACCOUNTS] = {0};
 
-// Removeing all non zero digits after 2 places from decimal point
-double round2Digits(double x){
-  x = (int)(x*100);
-  return x/100.0;
-}
 
-// return the first empty place or -1 if no space
-int findPlace(){
-  for(int i = 0; i < ACCOUNTS; i++){
-    if(acc[ACC_STT][i] == CLOSE)
-      return i + FIRST_ACC;
-  }
-  return WRONG_INDX;
-}
+double acc[2][ACCOUNTS] = {0};
 
-// true iff 'account' is in the allowd range (901-950) AND active
-bool isActive(int account){
-  account -= FIRST_ACC;
-  return account >= 0 && account < ACCOUNTS && acc[ACC_STT][account] == OPEN;
-}
 
 // Helper for deposite() and withrawal(). return new balance or -1 not allowd
-double dep_withr(int account, double amount){
-  if(isActive(account) && acc[ACC_BLC][account-FIRST_ACC] + amount >= 0){
-    account -= FIRST_ACC;
-    acc[ACC_BLC][account] += amount;
-    return acc[ACC_BLC][account];
+double dep_withr(int account, double amount)
+{
+  if(isActive(account) && acc[ACCOUNT_BALANCE][account-FIRST_ACCOUNT] + amount >= 0)
+{
+    account -= FIRST_ACCOUNT;
+    acc[ACCOUNT_BALANCE][account] += amount;
+    return acc[ACCOUNT_BALANCE][account];
   }
   return WRONG_INDX;
 }
-double deposite(int account, double amount){
+double deposite(int account, double amount)
+{
   if(amount >= 0)
+  {
     return dep_withr(account, amount);
-  return WRONG_INDX;
+  }
+ return WRONG_INDX;
 }
 
 // open an account and return the account number or -1 if no space
-int openAcc(double amount){
+int openAcc(double amount)
+{
 
   int account = findPlace();
-  if(account != WRONG_INDX){
-    acc[ACC_STT][account - FIRST_ACC] = OPEN;
+  if(account != WRONG_INDX)
+   {
+    acc[ACCOUNT_STATUS][account - FIRST_ACCOUNT] = OPEN_ACCOUNT;
     double ans = deposite(account, amount);
-    if(ans == WRONG_INDX){
+    if(ans == WRONG_INDX)
+    {
       printf("I'm sorry. you can't deposite a negative ammpunt.\n");
     }
   }
@@ -86,48 +79,97 @@ int openAcc(double amount){
 }
 
 // return the current balance for the spesific account or -1 if account inactive
-double getBalance(int account){
+double getBalance(int account)
+  {
   if(isActive(account))
-    return acc[ACC_BLC][account-FIRST_ACC];
-  return WRONG_INDX;
+   {
+    return acc[ACCOUNT_BALANCE][account-FIRST_ACCOUNT];
+  }
+ return WRONG_INDX;
 }
 
-// substract the spesific amount from the spesific  account (if there id enugh)
-double withrawal(int account, double amount){
-  if(amount >= 0)
-    return dep_withr(account, -amount);
-  return WRONG_INDX;
-}
+
 
 // set the balance to 0 and the status to close
-bool closeAcc(int account){
-  if(isActive(account)){
-    account -= FIRST_ACC;
-    acc[ACC_STT][account] = CLOSE;
-    acc[ACC_BLC][account] = 0;
+bool closeAcc(int account)
+  {
+  if(isActive(account))
+   {
+    account -= FIRST_ACCOUNT;
+    acc[ACCOUNT_STATUS][account] = CLOSE_ACCOUNT;
+    acc[ACCOUNT_BALANCE][account] = 0;
     return true;
   }
   return false;
 }
 
 // increas the balance of all accounts by 'inter' percent
-void interest(double inter){
+void interest(double inter)
+  {
   double rate = (100 + inter)/100;
-  for(int i = 0; i < ACCOUNTS; i++){
-    if(acc[ACC_STT][i] == OPEN)
-      acc[ACC_BLC][i] = round2Digits(acc[ACC_BLC][i]*rate);
+  for(int i = 0; i < ACCOUNTS; i++)
+    {
+    if(acc[ACCOUNT_STATUS][i] == OPEN_ACCOUNT)
+     {
+      acc[ACCOUNT_BALANCE][i] = round2Digits(acc[ACCOUNT_BALANCE][i]*rate);
+     }
   }
 }
 // print all active accounts - number and balance
-void printAll(){
-  for(int i = 0; i < ACCOUNTS; i++){
-    if(acc[ACC_STT][i] == OPEN)
-      printf("Account number: %d. Balance: %g\n", i + FIRST_ACC, acc[ACC_BLC][i]);
+void printAll()
+{
+  for(int i = 0; i < ACCOUNTS; i++)
+    {
+    if(acc[ACCOUNT_STATUS][i] == OPEN_ACCOUNT)
+     {
+      printf("Account number: %d. Balance: %g\n", i + FIRST_ACCOUNT, acc[ACCOUNT_BALANCE][i]);
+     }
   }
 }
 
 // close all acounts
-void closeAll(){
+void closeAll()
+{
   for(int i = 0; i < ACCOUNTS; i++)
+    {
     closeAcc(i);
+    }
 }
+
+// return the first empty place or -1 if no space
+int findPlace(){
+  for(int i = 0; i < ACCOUNTS; i++)
+{
+    if(acc[ACCOUNT_STATUS][i] == CLOSE_ACCOUNT)
+     {
+      return i + FIRST_ACCOUNT;
+     }
+  }
+  return WRONG_INDX;
+}
+
+// Removeing all non zero digits after 2 places from decimal point
+double round2Digits(double x)
+{
+  x = (int)(x*100);
+  return x/100.0;
+}
+
+// true iff 'account' is in the allowd range (901-950) AND active
+bool isActive(int account)
+{
+  account -= FIRST_ACCOUNT;
+  return account >= 0 && account < ACCOUNTS && acc[ACCOUNT_STATUS][account] == OPEN_ACCOUNT;
+}
+
+
+// substract the spesific amount from the spesific  account (if there id enugh)
+double withrawal(int account, double amount)
+  {
+  if(amount >= 0)
+   {
+    return dep_withr(account, -amount);
+  }
+  return WRONG_INDX;
+}
+
